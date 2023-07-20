3fbd86197a01554d4d8568067104f2710e2de69e
Maksim <maximka5291@gmail.com>
2023-07-20 12:57:00 +0400

    add print labels

---
+++ utils/printersConst.js
@@ -0,0 +1,28 @@
+const printers = [
+  {
+    location: 'Сигаево',
+    department: 'Упаковка',
+    printer: 'TSC DA220 sigaevo upak'
+  },
+  {
+    location: 'Ст. Разина',
+    department: 'Упаковка',
+    printer: 'TSC DA220 razin upak'
+  },
+  {
+    location: 'Ст. Разина',
+    department: 'Склад',
+    printer: 'TSC DA220 centr sklad'
+  },
+  {
+    location: 'Ст. Разина',
+    department: 'Участок наградки',
+    printer: 'TSC DA220 nagradka sklad C'
+  },
+  {
+    location: 'Ст. Разина',
+    department: 'Цех металлоконструкций',
+    printer: 'TSC DA220 metallka upak'
+  },
+];
+module.exports = printers;
